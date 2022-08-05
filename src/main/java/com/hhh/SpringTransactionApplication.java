20c41314f950b10c0d88f1f363c9a3c20f1a1ab9
Huang HH <862674004@qq.com>
2022-08-05 16:05:14 +0800

    创建了一个springTransaction项目

---
+++ src/main/java/com/hhh/SpringTransactionApplication.java
@@ -0,0 +1,16 @@
+package com.hhh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author HHH
+ */
+@SpringBootApplication
+public class SpringTransactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTransactionApplication.class, args);
+    }
+
+}
